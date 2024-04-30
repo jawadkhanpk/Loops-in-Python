3afd608f0f1c845f90c800e7a25434eafa08a701
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-30 21:35:35 +0500

    Basic For Loop Syntax in Python

--- Loops-in-Python.py
+++ Loops-in-Python.py
@@ -1,1 +1,6 @@
-print("Hello World")
+# (fruits) variable is a list containing 3 kinds of fruit
+fruits = ["Apple", "Peach", "Mango"]
+
+# (fruit) here is a variable which holds current item while loop is running
+for fruit in fruits:
+    print(fruit)
