280190e63aa80227be81ea6600ab0e68f1abb187
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-05-02 21:43:41 +0500

    Adding number from 1 upto 100 using range function in loops

--- Using-For-Loop-with-Range-Function-in-Python.py
+++ Using-For-Loop-with-Range-Function-in-Python.py
@@ -5,3 +5,10 @@ for number in range(1, 10):
 # If we pass 3rd parameter in range it will count as step size in a loop
 for step in range(1, 10, 2):
     print(step)
+
+
+# Adding numbers from 1 upto 100
+total = 0
+for count in range(1 , 101):
+    total += count
+print(f"\nThe Sum of number from 1 upto 100 is: {total}")
