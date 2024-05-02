00f95821762fd7fcb7c49d21de73977e2b8ab75c
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-05-02 21:38:31 +0500

    Using For Loop with Range Function in Python

---
+++ Using-For-Loop-with-Range-Function-in-Python.py
@@ -0,0 +1,7 @@
+# A simple Python Program demonstrating For Loops with Range Function
+for number in range(1, 10):
+    print(number)
+
+# If we pass 3rd parameter in range it will count as step size in a loop
+for step in range(1, 10, 2):
+    print(step)
